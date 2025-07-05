98661e7e669b594a6eba17b346d4dc8787e52697
Evan McGrane <evanmcgranebb98@gmail.com>
2025-07-05 13:51:22 +0100

    feat: add block package

---
+++ packages/block/test/test.test.ts
@@ -0,0 +1,8 @@
+import { describe, it } from 'vitest'
+
+describe('test', () => {
+  it('test suit starts', () => {
+    // biome-ignore lint/suspicious/noConsole: <explanation>
+    console.log('success')
+  })
+})
